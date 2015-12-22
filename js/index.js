907694a6ca04403ecb2ce9cb5f3ed0e656cf18b5
Tatiana Cooper <tatianaco@mellanox.com>
2015-12-22 14:55:17 +0200

    scan 14

--- js/index.js
+++ js/index.js
@@ -56,10 +56,11 @@ var app = {
             // documentation said the syntax was this:
             // var scanner = window.PhoneGap.require("cordova/plugin/BarcodeScanner");
             // but playing with options, seems like it should be this:
-            var scanner = cordova.plugins.barcodeScanner; //window.cordova.require("cordova/plugin/BarcodeScanner");
+            //var scanner = cordova.plugins.barcodeScanner; //window.cordova.require("cordova/plugin/BarcodeScanner");
             //var scannerModule = require("./barcodescanner.js");
             //var scanner = new scannerModule();
-            //var scanner = window.cordova.require("plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js");
+            var scanner = window.cordova.require("phonegap-plugin-barcodescanner.BarcodeScanner");
+            
             scanner.scan(
                     function (result) {
                         alert("We got a barcode\n" +
