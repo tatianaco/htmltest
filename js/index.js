16c1e081a24b18a25324ac4e80beac8c633c34f1
Tatiana Cooper <tatianaco@mellanox.com>
2016-01-04 11:48:00 +0200

    academy 8

--- js/index.js
+++ js/index.js
@@ -105,7 +105,7 @@ var app = {
         	try {
         		//var inappbrowser = window.cordova.require('cordova-plugin-inappbrowser.inappbrowser');
         		console.log("WEB - inappbrowser installed successfully");
-        		cordova.InAppBrowser.open('https://academy.mellanox.com', '_blank');
+        		window.open('https://academy.mellanox.com', '_blank');
         	} catch(e) {
         		console.log("WEB - inappbrowser not working");
         		console.log("WEB - stack = " + e.stack);
