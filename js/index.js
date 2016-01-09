ab97103796c14ca9d85d8c3c113be19a7860dc40
Tatiana Cooper <tatianaco@mellanox.com>
2016-01-09 20:16:36 +0200

    u9

--- js/index.js
+++ js/index.js
@@ -109,6 +109,8 @@ var app = {
         		//console.log("WEB - inappbrowser installed successfully");
         		//window.cordova.InAppBrowser.open('https://academy.mellanox.com', '_system');
         		var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
+        		var md = cordovaRef.require("cordova/plugin_list").metadata;
+        		console.log("md = " + md);
         		console.log("cordovaRef = " + cordovaRef);
         		console.log("cordovaRef.InAppBrowser = " + cordovaRef.InAppBrowser);
         		//window.cordova.exec(function(){}, null, "InAppBrowser", "open", ["http://www.google.com/", "_system", "location=yes"]);
